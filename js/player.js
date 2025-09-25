1c117c11d37655c0774a9ae22ec1c5c702a43a66
Dmytro Solovei <dmytro.solovei@motorolasolutions.com>
2025-09-25 15:40:35 +0300

    Minor fix

--- js/player.js
+++ js/player.js
@@ -1095,7 +1095,7 @@ export class Player {
           countryMarker.on(
             "click",
             function (ev) {
-              L.DomEvent.stopPropagation(ev);
+              // L.DomEvent.stopPropagation(ev);
               this.addUserClickCountriesPlay(
                 countryCode,
                 countryBoundary,
@@ -1106,7 +1106,7 @@ export class Player {
           countryBoundary.on(
             "click",
             function (ev) {
-              L.DomEvent.stopPropagation(ev);
+              // L.DomEvent.stopPropagation(ev);
               this.addUserClickCountriesPlay(
                 countryCode,
                 countryBoundary,
@@ -1423,7 +1423,7 @@ export class Player {
     if (this.playerType === "userPlayer") {
       countryBoundary.off("click");
       countryBoundary.on("click", (ev) => {
-        L.DomEvent.stopPropagation(ev);
+        // L.DomEvent.stopPropagation(ev);
         this.addUserPlayerInitialCountrySelectionHandler(
           countryCode,
           countryBoundary,
@@ -1432,7 +1432,7 @@ export class Player {
       });
       countryMarker.off("click");
       countryMarker.on("click", (ev) => {
-        L.DomEvent.stopPropagation(ev);
+        // L.DomEvent.stopPropagation(ev);
         this.addUserPlayerInitialCountrySelectionHandler(
           countryCode,
           countryBoundary,
@@ -1952,7 +1952,7 @@ export class Player {
     countryBoundary.on(
       "mouseover",
       function (event) {
-        L.DomEvent.stopPropagation(event);
+        // L.DomEvent.stopPropagation(event);
         countryBoundary.setStyle(styleObject);
         countryBoundary.bringToFront();
       }.bind(this)
@@ -1963,7 +1963,7 @@ export class Player {
     countryBoundary.on(
       "mouseout",
       function (event) {
-        L.DomEvent.stopPropagation(event);
+        // L.DomEvent.stopPropagation(event);
         countryBoundary.setStyle(styleObject);
         countryBoundary.bringToBack();
       }.bind(this)
