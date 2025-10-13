31d4d465e86cde95566ce3819bab7e11e3b90217
Dmytro Solovei <dmytro.solovei@motorolasolutions.com>
2025-10-13 11:03:36 +0300

    Firebase init code

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBz5cGGomnSf6XR_SBN-mxVEslHQGDWnKs",
+  authDomain: "country-alliance-guesser.firebaseapp.com",
+  projectId: "country-alliance-guesser",
+  storageBucket: "country-alliance-guesser.firebasestorage.app",
+  messagingSenderId: "834183240701",
+  appId: "1:834183240701:web:78bef8f2f4c9f9b81a5794",
+  measurementId: "G-523TP6VSTM",
+};
+
+const app = initializeApp(firebaseConfig);
+console.log(app);
+const analytics = getAnalytics(app);
