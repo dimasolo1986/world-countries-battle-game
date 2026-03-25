f27cc72cc0e54eb1523154c81c53b3a21ce2a869
dimasolo1986 <solovei.dmytro@gmail.com>
2026-03-25 17:42:30 +0200

    Minor fix

--- js/views/gameRulesView.js
+++ js/views/gameRulesView.js
@@ -120,7 +120,7 @@ class gameRulesView {
       "To clear selected country alliances, click"
     ]
       }`;
-    this._gameRulesNameHeading.textContent = `${localization[model.worldCountries.language]["Game Rules"]
+    this._gameRulesNameHeading.textContent = `📄 ${localization[model.worldCountries.language]["Game Rules"]
       }`;
     this._gameRulesFourCountriesAllianceDescription.textContent = `${localization[model.worldCountries.language][
       "Choose one alliance of countries that includes four countries on the world map."
@@ -146,7 +146,7 @@ class gameRulesView {
       "When entering an opponent's trap country, he receives one of eight clues about the location of one of your countries: 1. Name of the country; 2. Capital of the country; 3. Region (Europe, America, Asia, Africa, Oceania) in which one of your countries is located; 4. Subregion (Central Europe, North America, etc.) in which one of your countries is located; 5. Coat Of Arms Image; 6. Flag Image; 7. Country's Outline On Map; 8. Photo From Country. You can configure to receive only text clues (country name, country capital, region, subregion) or visual clues (country coat of arms, country flag, country's outline on map, photo from country)."
     ]
       }`;
-    this._gameRulesImportant.textContent = `${localization[model.worldCountries.language]["Important!"]
+    this._gameRulesImportant.textContent = `ℹ️ ${localization[model.worldCountries.language]["Important!"]
       }`;
     this._gameRulesImportantDescription.textContent = `${localization[model.worldCountries.language][
       "Countries in the same alliance of countries must be united by borders. Alliances of countries must be separated from each other by at least one country's borders. An island country can only be used as an alliance with one country or as a trap country. If the player does not make an attempt to guess the opponent's country within one minute (can be changed), the turn passes to the opponent."
@@ -156,19 +156,19 @@ class gameRulesView {
       "The computer will also randomly select five bonus countries (can be changed), when hit, players get an extra attempt to guess the opponent's country and 10 extra points."
     ]
       }`;
-    this._gameRulesWithFriendHeader.textContent = `${localization[model.worldCountries.language]["Play With A Friend Mode"]
+    this._gameRulesWithFriendHeader.textContent = `🧓 ${localization[model.worldCountries.language]["Play With A Friend Mode"]
       }`;
     this._gameRulesWithFriendDescription.textContent = `${localization[model.worldCountries.language][
       "To play with your friend, you need to select the game mode with a friend on the main page, create a game room and a link to the game for your friend and send it to him. Follow the connection status of your friend at the top of the screen after starting the game. There is also the possibility of communicating with your friend via chat at the bottom of the screen: you can greet him, wish him a good game or give yourself hints about your countries and alliances of countries (chat may be hidden by the bottom browser bar, so open it full screen or hide this bar)."
     ]
       }`;
-    this._gameRulesScoreHeading.textContent = `${localization[model.worldCountries.language]["Score"]
+    this._gameRulesScoreHeading.textContent = `🏅 ${localization[model.worldCountries.language]["Score"]
       }`;
     this._gameRulesScoreDescription.textContent = `${localization[model.worldCountries.language][
       "If a player guesses an alliance of countries that contains four countries, he gets 15 points, three countries - 25 points, two countries - 35 points, one country - 50 points. When falling into an opponent's trap country, the player loses 10 points the first time, 20 points the second time, 30 points the third time and 50 points the fourth time. At the end of the game, the player gets an additional 10 points for each alliance of countries that was not guessed by the opponent."
     ]
       }`;
-    this._gameVideos.textContent = `${localization[model.worldCountries.language]["VIDEOS:"]
+    this._gameVideos.textContent = `📼 ${localization[model.worldCountries.language]["VIDEOS:"]
       }`;
     this._gameRulesCountryAllianceSelectionVideoTitle.textContent = `${localization[model.worldCountries.language][
       "Country Alliances Selection — Video Tutorial"
